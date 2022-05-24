c06a1d50e281c0ac9efc4caea523a03d961d374e
Sani Letchu <sani.letchu@gmail.com>
2022-05-24 14:28:46 +0300

    Added a info function to book constructor that returns information about the book

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages
   this.read = read;
+  this.info = function() {
+      let readOrNot = "not read yet";
+      if(this.read) {
+          readOrNot = "is read";
+      }
+      return this.title + " by " + this.author + ", " + this.pages + " pages, " + readOrNot;
+  }
 }
 
 function addBookToLibrary() {
