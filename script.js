3acb8b859a6300d9da0b8b75b24815bb4ae611b5
Sani Letchu <sani.letchu@gmail.com>
2022-05-24 15:57:01 +0300

    Added some header, main and footer content

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ function addBookToLibrary(title, author, pages, read) {
   let book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+//Some books for the library
+addBookToLibrary("A Game of Thrones", "George R. R. Martin", 694, false);
+addBookToLibrary("A Clash of Kings", "George R. R. Martin", 768, false);
+addBookToLibrary("A Storm of Swords", "George R. R. Martin", 973, false);
+addBookToLibrary("A Feast for Crows", "George R. R. Martin", 753, false);
+addBookToLibrary("A Dance with Dragons", "George R. R. Martin", 1056, false);
+
+function displayLibrary() {
+
+}
